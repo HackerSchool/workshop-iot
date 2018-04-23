11cbb92d6f1ef3fb0758d5b8d889a9eb3abcf2a6
Francisco Mendes <fsmendes3@gmail.com>
2018-04-23 12:20:31 +0100

    Add files via upload

---
+++ Exercícios/5º Exercício/5_exercicio_-_Introducao_ao_Blynk_-_solucao/5_exercicio_-_Introducao_ao_Blynk_-_solucao.ino
@@ -0,0 +1,87 @@
+/*************************************************************
+  Download latest Blynk library here:
+    https://github.com/blynkkk/blynk-library/releases/latest
+
+  Blynk is a platform with iOS and Android apps to control
+  Arduino, Raspberry Pi and the likes over the Internet.
+  You can easily build graphic interfaces for all your
+  projects by simply dragging and dropping widgets.
+
+    Downloads, docs, tutorials: http://www.blynk.cc
+    Sketch generator:           http://examples.blynk.cc
+    Blynk community:            http://community.blynk.cc
+    Follow us:                  http://www.fb.com/blynkapp
+                                http://twitter.com/blynk_app
+
+  Blynk library is licensed under MIT license
+  This example code is in public domain.
+
+ *************************************************************
+
+  This sketch shows how to read values from Virtual Pins
+
+  App project setup:
+    Slider widget (0...100) on Virtual Pin V1
+ *************************************************************/
+
+/* Comment this out to disable prints and save space */
+#define BLYNK_PRINT Serial
+
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "355c34dade3248c3948e990932ed3224";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "Chico";
+char pass[] = "12345678";
+
+int pinValueV1;
+int sliderValue;
+
+// This function will be called every time Slider Widget
+// in Blynk app writes values to the Virtual Pin V1
+BLYNK_WRITE(V0) {
+  pinValueV1 = param.asInt(); // Atribui o valor do pino V1 para uma variavel
+  Serial.println(pinValueV1);
+}
+
+
+// This function will be called every time Slider Widget
+// in Blynk app writes values to the Virtual Pin V1
+BLYNK_WRITE(V1) {
+  sliderValue = param.asInt(); // assigning incoming value from pin V1 to a variable
+
+  // process received value
+  Serial.println(sliderValue);
+
+}
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+
+  // Inicializa o LED integrado como um pino de saída
+  pinMode(LED_BUILTIN, OUTPUT); 
+
+  Blynk.begin(auth, ssid, pass);
+}
+
+void loop()
+{
+  Blynk.run();
+  
+  // processa o valor do pin V1
+  if(pinValueV1 == 0) {
+    digitalWrite(LED_BUILTIN, LOW); // Liga o LED 
+  }
+  else {
+    digitalWrite(LED_BUILTIN, HIGH); // Desliga o LED
+  }
+}
+
---
+++ Exercícios/7º Exercício/7_exercicio_-_controlo_lcd_-_imagem_-_solucao/7_exercicio_-_controlo_lcd_-_imagem_-_solucao.ino
@@ -0,0 +1,47 @@
+/*
+ * Workshop IOT - HackerSchool
+ * 6º Exercício - Controlo LCD - imagens - solução
+ * 
+ 
+ Biblioteca:
+ void drawXbm(int16_t x, int16_t y, int16_t width, int16_t height, const char* xbm);
+ 
+ * 
+*/
+
+
+#include "SSD1306.h" // Biblioteca de controlo do LCD SSD1306
+#include "OLEDDisplayUi.h" // Biblioteca de UI
+
+// Incluir ficheiro de imagens
+#include "Imagem.h"
+
+// I2C_DISPLAY_ADDRESS, SDA_PIN, SCL_PIN
+SSD1306  display(0x3c, D1, D2);
+OLEDDisplayUi   ui( &display );
+
+void setup() {
+  // Inicia a comunicação I2C com o LCD
+  display.init();
+
+  //Vira o ecrã ao contrário
+  display.flipScreenVertically();
+
+  //Define o contraste do ecrã para o máximo
+  display.setContrast(255);
+
+  // Limpa o conteúdo do ecrã
+  display.clear();
+
+  // Desenha a imagem
+  display.drawXbm(0, 0, HS_Width, HS_Height, HS_logo_bitmap);
+
+  // Atualiza o ecrã
+  display.display();
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
